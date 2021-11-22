5e0cf6aa71dc67f5e44c6e34c59a5ed39900928f
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2021-11-23 05:10:38 +0530

    Create 2randomtext.java

---
+++ 2randomtext.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args){
+        System.out.println("Hello World!");
+        System.out.println("Prepinsta is Awesome");
+        System.out.println("Welcome to the java course");
+        System.out.println("If you want to learn Java in depth, complete this playlist.");
+        
+    }
+}
